b179dd18cc3f536b30ec14016790ff0d1e8d78ed
Kyryl Feshchenko <48104398+javaAndScriptDeveloper@users.noreply.github.com>
2025-02-04 20:18:02 +0200

    Initial commit

---
+++ src/main/java/com/example/company/config/BaseConfig.java
@@ -0,0 +1,6 @@
+package com.example.company.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BaseConfig {}
---
+++ src/main/java/com/example/company/exception/ApplicationException.java
@@ -0,0 +1,43 @@
+package com.example.company.exception;
+
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class ApplicationException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+    private final Optional<String> errorMessageOptional;
+    private final Optional<Exception> internalExceptionOptional;
+
+    ApplicationException() {
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+        this.errorMessageOptional = Optional.empty();
+        this.internalExceptionOptional = Optional.empty();
+    }
+
+    ApplicationException(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+        this.errorMessageOptional = Optional.empty();
+        this.internalExceptionOptional = Optional.empty();
+    }
+
+    public ApplicationException(HttpStatus httpStatus, String errorMessage) {
+        this.httpStatus = httpStatus;
+        this.errorMessageOptional = Optional.of(errorMessage);
+        this.internalExceptionOptional = Optional.empty();
+    }
+
+    ApplicationException(HttpStatus httpStatus, String errorMessage, Exception runtimeException) {
+        this.httpStatus = httpStatus;
+        this.errorMessageOptional = Optional.of(errorMessage);
+        this.internalExceptionOptional = Optional.of(runtimeException);
+    }
+}
